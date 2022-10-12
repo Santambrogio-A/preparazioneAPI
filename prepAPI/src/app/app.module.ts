a69cbff3d12c10554b676a0114fb36b95a540038
Santambrogio-A <santambrogio.andrea@iisgalvanimi.edu.it>
2022-10-12 15:47:50 +0000

    Punto 3

--- prepAPI/src/app/app.module.ts
+++ prepAPI/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
@@ -13,6 +13,7 @@ import { SearchComponent } from './search/search.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
